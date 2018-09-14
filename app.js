3f3e9ae0221925822413594f66bb1935b50f88b5
Anirban Bera <anirban.bera.89@gmail.com>
2018-09-14 23:58:25 +0530

    added comments

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 (function () {
 
-    var vm = {};
+    var vm = {};//short form of view model
     var els = document.querySelectorAll('[twoWay]');
     // console.log(els);
 
+    //this hook will run each time you add or update anything in viewmodel
     var vmBindingHook = function (propName) {
         if (!vm.hasOwnProperty(propName)) {
-            var val;
+            var val; //required to store the new val entered by user
             Object.defineProperty(vm, propName, {
                 set: function (newVal) {
                     val = newVal;
                     console.log('inside define prop');
+                    //update all dom elements related to the property of view model
                     els.forEach(function (el) {
                         if (el.getAttribute('twoWay') === propName) {
                             if (el.type === 'text') {
@@ -21,7 +23,7 @@
                         }
                     });
                 },
-                get: function(){
+                get: function(){ //required to show the actual propery of propName in the object
                     return val;
                 },
                 enumerable: true
@@ -36,6 +38,7 @@
         if (el.type === 'text') {
             console.log(el);
             vmBindingHook(propName);
+            //update the view model each time you are changing something
             el.onkeyup = function () {
                 vm[propName] = el.value;
                 console.log(vm);
